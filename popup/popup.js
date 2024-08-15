0e59fd83b0e118d205054f8bed32451e1632e65f
rlagusals1102 <hmkim112@naver.com>
2024-08-16 08:38:02 +0900

    [feat] 데이터 전송 기능 구현 완료

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('toggle');
-
-    // 토글 버튼 상태에 따라 백그라운드 스크립트에 메시지 전송
-    toggle.addEventListener('change', function() {
-        const enabled = toggle.checked;
-        chrome.runtime.sendMessage({ enabled: enabled });
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#btn1').addEventListener('click', () => {
+        try {
+            chrome.runtime.sendMessage({ action: 'activate' });
+        } catch (e) {}
     });
 });
